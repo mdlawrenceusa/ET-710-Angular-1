c33f6005a68042b8e7159c4b66c0b98a805868bb
mdlawrenceusa <mdlawrence@me.com>
2020-04-27 16:01:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wffxrs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wffxrs)
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+import {
+  AccordionModule,
+  AlertModule,        // Foundation Callouts
+  ButtonsModule,
+  CarouselModule,     // Foundation Orbit
+  CollapseModule,
+  BsDatepickerModule,
+  BsDropdownModule,   // Foundation Dropdown Menus and Dropdown Panes
+  ModalModule,        // Foundation Reveal
+  OffcanvasModule,
+  PaginationModule,
+  ProgressbarModule,
+  RatingModule,
+  SortableModule,
+  TabsModule,
+  TimepickerModule,
+  TooltipModule,
+  TypeaheadModule,
+} from 'ngx-foundation';
+
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, AlertModule.forRoot() ] ,
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
